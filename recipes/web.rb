ea7ec4c654b10279eb906f19876bef6a3bc3b275
Gerhard Lazu <gerhard@lazu.co.uk>
2011-11-25 17:38:57 +0000

    python::uwsgi handles uwsgi setup
    The wsgi.py file was an idea from here: http://www.jeremybowers.com/blog/post/5/django-nginx-and-uwsgi-production-serving-millions-page-views/
    Didn't really work...

--- recipes/web.rb
+++ recipes/web.rb
@@ -11,3 +11,22 @@ python_pip "graphite_web" do
   directory "#{node.graphite.home}/webapp"
 end
 
+file "/opt/graphite/webapp/wsgi.py" do
+  content %{import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'graphite.settings'
+
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
+  }
+  mode "0750"
+end
+
+execute "correct permissions for graphite web" do
+  command "chown -fR graphite.uwsgi #{node.graphite.home}/webapp"
+end
+
+include_recipe "python::uwsgi"
