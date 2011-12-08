dfa1dfcde79770438c4aee361fbf52f59bb98544
Gerhard Lazu <gerhard@lazu.co.uk>
2011-12-08 17:17:31 +0000

    Metric collectors are now bash scripts (with optional dependencies)

--- recipes/metrics.rb
+++ recipes/metrics.rb
@@ -23,34 +23,44 @@ def one_but_last(array)
   array[-2]
 end
 
-node.graphite.metrics.files.each do |metric|
-  metric_name = one_but_last(metric[:filename].split("."))
-
-  service "metric-#{metric_name}" do
+node.graphite.metrics.collectors.each do |collector|
+  service "metric-#{collector[:name]}" do
     supports :start => true, :stop => true, :restart => true
     provider Chef::Provider::Service::Upstart
   end
 
-  cookbook_file "/home/#{node.graphite.metrics.user}/#{metric[:filename]}" do
-    cookbook (metric[:cookbook] || "graphite")
-    source "metrics/#{metric[:filename]}"
+  collector_file = collector[:file] || "#{collector[:name]}.sh"
+  cookbook_file "/home/#{node.graphite.metrics.user}/#{collector_file}" do
+    cookbook (collector[:cookbook] || "graphite")
+    source "metrics/#{collector_file}"
     owner node.graphite.metrics.user
     group node.graphite.metrics.user
-    mode 0644
-    notifies :restart, resources(:service => "metric-#{metric_name}"), :delayed
+    mode 0755
   end
 
-  template "/etc/init/metric-#{metric_name}.conf" do
+  if collector[:depends]
+    collector[:depends].each do |script|
+      cookbook_file "/home/#{node.graphite.metrics.user}/#{script}" do
+        cookbook (collector[:cookbook] || "graphite")
+        source "metrics/#{script}"
+        owner node.graphite.metrics.user
+        group node.graphite.metrics.user
+        mode 0644
+      end
+    end
+  end
+
+  template "/etc/init/metric-#{collector[:name]}.conf" do
     cookbook "graphite"
     source "metrics/metric.upstart.erb"
     variables(
-      :name    => metric_name,
-      :command => metric[:command]
+      :name   => collector[:name],
+      :script => collector_file
     )
     owner "root"
     group "root"
     mode "0644"
     backup false
-    notifies :restart, resources(:service => "metric-#{metric_name}"), :delayed
+    notifies :restart, resources(:service => "metric-#{collector[:name]}"), :delayed
   end
 end
