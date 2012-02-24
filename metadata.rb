b8836abce2b18ae4e23ac05bc6f80a6b5ed4f821
Gerhard Lazu <gerhard@lazu.co.uk>
2012-02-24 16:11:04 +0000

    Depending cookbook location update

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,6 @@ recipe "graphite", "Installs all 3 Graphite components: whisper, carbon & web"
 
 supports "ubuntu"
 
-depends "python"    # https://github.com/gchef/python-cookbook
+depends "python"    # https://github.com/gosquared/python-cookbook
 depends "bootstrap" # https://github.com/gchef/bootstrap
 # depends "apache2"
